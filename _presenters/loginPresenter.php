fcf37c006f88326b519311def31c7f97bb57df45
LeafDev <leafdev@github.com>
2018-10-15 15:07:12 +0800

    Added flashcard on the login

--- _presenters/loginPresenter.php
+++ _presenters/loginPresenter.php
@@ -1,37 +1,40 @@
 <?php
 class loginPresenter {
-    // HTTP Header Method: GET
-    // Used to retrive a data or a view
+
+	public static function displayError(){
+		\Plugins\FlashCard::setFlashCard("Authentication Error", "/login", "Invalid Username or Password");
+		header("location: /login");
+		exit;
+	}
+
     public function get(){
         View::addVar("view_title", "AMFM Login");
         View::addVar("BODY_CLASS", "bg-light");
         View::addCSS("/_layouts/login/login.css");
         View::addCSS("http://".Route::domain()."/css/".md5("Bootstrap").".min.css");
+
+        View::addVar("fc", Plugins\FlashCard::getFlashCard()[0]);
     }
 
-    // HTTP Header Method: POST
-    // Usually used when to insert a new data
     public function post(){
         Params::permit('usn', 'pwd');
 
         $usn = Params::get('usn');
         $pwd = Params::get('pwd');
-        $invalid = 'Invalid Username and Password';
 
         // Check if all form is may laman
         if (!isset($usn, $pwd)){
-            echo $invalid;
-            exit;
+        	self::displayError();
         }
+
         // Check the length of Usn
         if(strlen($usn) < 3 || strlen($usn) > 32){
-            echo $invalid;
-            exit;
+	        self::displayError();
         }
+
         // Check the length of password
         if(strlen($pwd) < 3 || strlen($pwd) > 32){
-            echo $invalid;
-            exit;
+	        self::displayError();
         }
 
         // Start Account checking...
@@ -47,18 +50,13 @@ class loginPresenter {
         }
 
         // Show the invalid error
-        echo $invalid;
-        exit;
+	   self::displayError();
     }
 
-    // HTTP Header Method: PUT
-    // Usually used when about to update a data
     public function put(){
         Route::returnCode(401);
     }
 
-    // HTTP Header Method: DELETE
-    // Usually used when about to delete a data
     public function delete(){
         Route::returnCode(401);
     }
